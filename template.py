033cee4fa25173595dff0182109b8122c0f3d7d3
rithiklogu <bazeer33333@gmail.com.com>
2024-08-05 15:59:22 +0530

    folder structer added

--- template.py
+++ template.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 project_name = 'us_visa'
 list_of_files=[
-    f'{project_name}/__init__.py',
-    f'{project_name}/componets/__init__.py',
-    f'{project_name}/componets/data_ingestion.py',
-    f'{project_name}/components/data_validation.py',
-    f'{project_name}/components/data_transformation.py',
-    f'{project_name}/compnents/model_trainer.py',
-    f'{project_name}/components/model_evaluation.py',
-    f'{project_name}/components/model_pusher.py',
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
     f'{project_name}/configuraction/__init__.py',
     f'{project_name}/constants/__init__.py',
     f'{project_name}/entity/__init__.py',
