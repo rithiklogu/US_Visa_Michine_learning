2a580b26387f8993338b6f8118135c5be15e39d9
rithiklogu <bazeer33333@gmail.com.com>
2024-08-05 15:53:40 +0530

    folder strucue added

---
+++ template.py
@@ -0,0 +1,37 @@
+import os 
+from pathlib import Path
+
+project_name = 'us_visa'
+list_of_files=[
+    f'{project_name}/__init__.py',
+    f'{project_name}/componets/__init__.py',
+    f'{project_name}/componets/data_ingestion.py',
+    f'{project_name}/components/data_validation.py',
+    f'{project_name}/components/data_transformation.py',
+    f'{project_name}/compnents/model_trainer.py',
+    f'{project_name}/components/model_evaluation.py',
+    f'{project_name}/components/model_pusher.py',
+    f'{project_name}/configuraction/__init__.py',
+    f'{project_name}/constants/__init__.py',
+    f'{project_name}/entity/__init__.py',
+    f'{project_name}/entity/config_entity.py',
+    f'{project_name}/entity/artifact_entity.py',
+    f'{project_name}/exepection/__init__.py',
+    f'{project_name}/logger/__init__.py',
+    f'{project_name}/pipline/__init__.py',
+    f'{project_name}/pipline/training_pipeline.py',
+    f'{project_name}/pipline/predection_pipeline.py',
+    f'{project_name}/utils/__init__.py',
+    f'{project_name}/utils/main_utils.py'
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at:{filepath}")
