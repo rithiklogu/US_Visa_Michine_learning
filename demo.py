9848b0a6a3360d2b5d03dc143129c7e2958eb593
rithiklogu <bazeer33333@gmail.com.com>
2024-09-16 12:35:16 +0530

    exepection file updated

--- demo.py
+++ demo.py
@@ -1,3 +1,3 @@
 from us_visa.logger import logging
-
+from us_visa.exepection import USvisaException
 logging.info("hi bhuvi's this is rithik")
--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -4,14 +4,17 @@ import os
 from from_root import from_root
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_dir = 'logs'
 
-logs_path = os.path.join(from_root(),log_dir, LOG_FILE)
-os.makedirs(log_dir, exist_ok = True)
+logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
 
-logging.basicConfig(filename=logs_path,
-                    format='[%(asctime)s] %(name)s %(levelname)s %(message)s',
-                    level = logging.DEBUG,
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    filename=logs_path,
+    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
 )
